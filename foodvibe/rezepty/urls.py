cae8c48be4b9d72977f81f6c53483f4923da428a
МЭЭРИЗАТ <>
2025-06-02 17:32:14 +0300

    new project7

--- foodvibe/rezepty/urls.py
+++ foodvibe/rezepty/urls.py
@@ -1,17 +1,23 @@
 from django.urls import path
 from .views import *
 
-
 urlpatterns = [
     path('', RecipeViewSet.as_view({'get': 'list',
                                  'post': 'create'}), name='recipe_list'),
     path('<int:pk>/', RecipeDetailViewSet.as_view({'get': 'retrieve',
                                           'put': 'update', 'delete': 'destroy'}), name='recipe_detail'),
+    path('category/', CategoryFoodViewSet.as_view({'get': 'list',
+                                    'post': 'create'}), name='category_list'),
+    path('type/', TypeViewSet.as_view({'get': 'list',
+                                                   'post': 'create'}), name='type_list'),
+    path('mealplan/', MealPlanListCreateViewSet.as_view({'get': 'list',
+                                       'post': 'create'}), name='mealplan_list'),
     path('favorite/', FavoriteViewSet.as_view({'get': 'list',
                                  'post': 'create'}), name='favorite_list'),
     path('favorite/<int:pk>/', RecipeDetailViewSet.as_view({'get': 'retrieve',
                                           'put': 'update', 'delete': 'destroy'}), name='recipe_detail'),
-
+    path('', RecipeViewSet.as_view({'get': 'list',
+                                    'post': 'create'}), name='recipe_list'),
     path('comment/', CommentViewSet.as_view({'get': 'list',
                                  'post': 'create'}), name='comment_list'),
     path('comment/<int:pk>/', CommentViewSet.as_view({'get': 'retrieve',
